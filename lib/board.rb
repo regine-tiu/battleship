2c5cb39af715b5c7038126f028fdbd6e1cc44fce
regine-tiu <regine.d.tiu@gmail.com>
2021-05-12 21:58:09 +0800

    part 1 solved

--- lib/board.rb
+++ lib/board.rb
@@ -18,10 +18,56 @@ attr_reader :size
 
   def num_ships
     count = 0
-    
     @grid.flatten.each do |ele|
       count += 1 if ele == :S 
     end   
     count  
   end
+
+  def attack(position)
+    if self[position] == :S
+      self[position] = :H
+      puts "you sunk my battleship"
+      true
+    else
+      self[position] = :X
+      false
+    end 
+  end  
+
+  def place_random_ships
+    rando_ships = (@size * 0.25).round
+    while self.num_ships < rando_ships
+      rando_row = rand(0...@grid.length)
+      rando_col = rand(0...@grid.length)
+      pos = [rando_row, rando_col]
+      self[pos] = :S
+    end  
+  end  
+
+  def hidden_ships_grid
+    @grid.map do |row|
+      row.map do |ele|
+        if ele == :S
+          :N
+        else
+          ele  
+        end  
+      end
+    end  
+  end 
+
+  def self.print_grid(grid)
+    grid.each do |row|
+      puts row.join(" ")
+    end  
+  end  
+
+  def cheat
+    Board.print_grid(@grid)
+  end  
+
+  def print
+    Board.print_grid(hidden_ships_grid)
+  end  
 end
