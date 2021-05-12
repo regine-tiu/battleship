b4c98a2d8e6a1a02714543dd9990c75e411c1c80
regine-tiu <regine.d.tiu@gmail.com>
2021-05-12 21:13:49 +0800

    part 1 solved

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,27 @@
 class Board
-  
+attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) {Array.new(n, :N)}
+    @size = n * n
+  end  
+
+  def [](position)
+    row, col = position
+    @grid[row][col]
+  end  
+
+  def []=(position, val)
+    row, col = position
+    @grid[row][col] = val
+  end  
+
+  def num_ships
+    count = 0
+    
+    @grid.flatten.each do |ele|
+      count += 1 if ele == :S 
+    end   
+    count  
+  end
 end
